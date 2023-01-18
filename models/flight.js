bbd98ba333c7973d7a1a746c7771136758835aed
bcstidd <bc.stidd@gmail.com>
2023-01-18 14:25:12 -0800

    Completion of Lab 1

---
+++ models/flight.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    arrival: Date,
+})
+const flightSchema = new Schema({
+    airline:{
+        type:String,
+        enum: ['American', 'Southwest', 'Alaska', 'United']
+    } ,
+    airport:{
+        type:String,
+        enum: ['AUS','DFW','DEN','LAX','SAN']
+    },
+    destinations:{
+        type:[destinationSchema]
+    },
+    flightNo:{
+        type:Number,
+        min:10,
+        max:9999
+    },
+    departs:Date
+}, {
+    timestamps:true
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
